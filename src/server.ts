db419a44f50f06d33594be49e7e000688acda0c5
Danilo Araújo <daniloaraujo1990@hotmail.com>
2020-06-03 01:41:17 -0300

    Personalizando scripts para execução

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,12 @@ const app = express();
 app.get('/users', (request, response) => {
     console.log("Listagem de usuários!");
 
-    response.send('<h3>Hello World!</h3>')
+    response.send('<h3>Hello World!</h3> <br> <h5>Testing!</h5>');
+
+    response.json([
+        'Danilo',
+        'Lucas'
+    ]);
 });
 
 app.listen(3333)
